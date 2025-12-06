10dba56fe979b2bffc8f3511cfa77b0b3974b10e
DedaNik <121578686+DedaNik@users.noreply.github.com>
2025-12-06 19:10:43 +0300

    Update train.py

--- src/train.py
+++ src/train.py
@@ -4,26 +4,44 @@ import numpy as np
 from catboost import CatBoostClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import StratifiedKFold
 
 def data_separation(EDA: pd.DataFrame,y_name: str):
   X = EDA.drop(columns=[y_name],axis=1)
   y = EDA[y_name]
-  X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=42)
-  return X_train, X_test, y_train, y_test
+  return X, y
 
-def train_catboost(X_train, X_test, y_train, y_test):
-  clf = CatBoostClassifier(
+def train_catboost(X,y):
+  
+  skf = StratifiedKFold(n_splits=5, shuffle = True, random_state=42)
+  auc_scores = []
+  
+  for fold, (train_idx, val_idx) in enumerate(skf.split(X,y),1):
+
+    X_train, X_test = X.iloc[train_idx] , X.iloc[val_idx]
+    y_train, y_test = y.iloc[train_idx] , y.iloc[val_idx]
+
+    clf = CatBoostClassifier(
      verbose=100,   # выводит прогресс каждые 100 итераций
     random_state=42
-  )
+    )
+    
+    clf.fit(X_train, y_train)
+    y_pred_proba = clf.predict_proba(X_test)[:, 1]
+    
+    auc = roc_auc_score(y_test, y_pred_proba)
+    print(f"Fold {fold} AUC: {auc:.4f}")
+    auc_scores.append(auc)
+    clf.save_model(f'models/catboost_fold{fold}.cbm')
 
-  clf.fit(X_train, y_train,)
-  y_pred_proba = clf.predict_proba(X_test)[:, 1]
-  
-  auc = roc_auc_score(y_test, y_pred_proba)
-  print(f"AUC: {auc:.4f}")
+  results = pd.DataFrame({
+    'Fold': list(range(1, 6)),
+    'AUC': auc_scores
+  })
 
-  clf.save_model('models/catboost_baseline.cbm')
-  
+  results = results.append({'Fold':'Mean','AUC':np.mean(auc_scores)}, ignore_index=True)
+
+  print(results)
+    
   return clf
 
